00812cc7eae2a778193018b3cb5e0fc3144b76c3
reiyayakko <reiyayakko@gmail.com>
2021-01-27 21:29:36 +0900

    fix: change prng for limit for unifrom

--- src/util/string/rand-str.ts
+++ src/util/string/rand-str.ts
@@ -1,5 +1,5 @@
 
-import { rand } from "number/random/default";
+import { random } from "number/random/random";
 import { toBaseN } from "number/util/basen";
 import { MAX_BIT_NUMBER } from "number/util/constant";
 
@@ -12,7 +12,7 @@ export const randStr = (length?: number): string => {
     const len = length! < MAX_BIT_NUMBER ? Math.abs(length!) | 0 : 8;
 
     let result = "";
-    do result += toBaseN(rand(), 62);
+    do result += toBaseN(random(916132832) | 0, 62);
     while(result.length <= len);
     return result.slice(-len);
 };
