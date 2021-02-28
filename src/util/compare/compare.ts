7865b4e39a5459f99c9cd07730f6b2a754847dee
reiyayakko <reiyayakko@gmail.com>
2021-03-01 03:42:27 +0900

    feat: add support for compare with gt and lt

--- src/util/compare/compare.ts
+++ src/util/compare/compare.ts
@@ -1,7 +1,14 @@
 
 import { isNumber } from "number/is/number";
-import { isFunction } from "util/is/function";
-import type { Comparator } from "./types";
+import { isFunction } from "../is/function";
+import type { Comparator, ComparatorResult } from "./types";
+
+const comparatorRsltToBool = (rslt: ComparatorResult): boolean => (
+    isNumber(rslt) ? rslt < 0
+    : rslt === "<" ? true
+    : rslt === ">" ? false
+    : Boolean(rslt)
+);
 
 /**
  * Compare values with comparator and returns boolean value.
@@ -10,10 +17,11 @@ import type { Comparator } from "./types";
  * @param right value to compare on right.
  * @param comparator comparison function or boolean value, true if less than, false if greater than.
  */
-export const compare = <T>(left: T, right: T, comparator: Comparator<T>=true): boolean => {
-    const dir = (isFunction(comparator)
-        ? comparator(left, right)
-        : comparator ? left < right : left > right
-    );
-    return isNumber(dir) ? dir < 0 : Boolean(dir);
-};
+export const compare = <T>(left: T, right: T, comparator: Comparator<T>=true): boolean => (
+    isFunction(comparator) ? comparatorRsltToBool(comparator(left, right))
+    : comparator === "<" ? left < right
+    : comparator === "<=" ? left <= right
+    : comparator === ">" ? left > right
+    : comparator === ">=" ? left >= right
+    : comparator ? left < right : left > right
+);
